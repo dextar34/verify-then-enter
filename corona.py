549869d7633eca4343de53f78217928290c701f2
dextar34 <76518074+dextar34@users.noreply.github.com>
2021-01-04 15:24:26 +0600

    Add files via upload

---
+++ corona.py
@@ -0,0 +1,10 @@
+print('Enter your name')
+name=input("")
+print('Enter your age')
+age=input("")
+print('Do you have corona yes/no')
+corona=input('')
+if corona=="yes" :
+    print(f"{name} can't enter the building")
+else :
+    print(f"{name} can enter the building")
